d748375b8310b4e7e86e36c933e33f232398e0e8
jthae <40273807+jthae@users.noreply.github.com>
2019-02-19 22:25:52 -0600

    Added unedited files

---
+++ Todo.java
@@ -0,0 +1,48 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-02-19
+OU ID_______________: 113397751
+OU EMAIL____________: jayciejthaemert@ou.edu
+
+Tasks:
+1. calculator calculateTwoTokens
+2. calculator calculateThreeTokens
+3. calculator execute
+4. calculator parseAndExecute
+5. test validHalve
+6. test twoTokensInvalidCommand
+7. test threeTokensValidAdd
+8. test threeTokensValidSubtract
+9. test threeTokensValidDivide
+10. test threeTokensInvalidCommand
+11. test execute commands
+12. test parseAndExecute commands
+
+Estimated Time (minutes):
+1. 15
+2. 10
+3. 10
+4. 10
+5. 5
+6. 5
+7. 5
+8. 5
+9. 5
+10. 5
+11. 10
+12. 10
+
+Actual Time (minutes):
+1. 15
+2. 5
+3. 20
+4. 10
+5. 3
+6. 3
+7. 5
+8. 3
+9. 3
+10. 3
+11. 10
+12. 8
+*/}
